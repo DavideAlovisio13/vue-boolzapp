c5d9798e6f88e5d17b03f02c1bbdd6b81b1a11ec
DavideAlovisio13 <alovisiod@gmail.com>
2024-04-04 14:03:12 +0200

    (js) add filter and date

--- js/main.js
+++ js/main.js
@@ -1,20 +1,45 @@
 
 import { contacts } from './data.js';
 const { createApp } = Vue;
+const lux = luxon.DateTime;
 
 createApp({
     data() {
         return {
             contacts: contacts,
-            activeContactId: 1
+            activeContactId: 1,
+            messageText: '',
+            searchText: ''
         }
     },
     methods:{
-        
+        setActive (id){
+            this.activeContactId = id;
+        },
+        createMex(msg, status){
+            const newMex = {
+                date: lux.now().setLocale('it').toFormat('dd/MM/yyyy HH:mm:ss'),
+                message: msg,
+                status:status
+            }
+            return newMex;
+        },
+        sentMex(){
+            const newMex = this.createMex(this.messageText, 'sent');
+            this.activeContact.messages.push(newMex);
+            this.messageText = '';
+            setTimeout(() => {
+                const newMex = this.createMex('ok', 'received');
+                this.activeContact.messages.push(newMex);
+            },1000);
+        },
     },
     computed:{
-        activeContactId() {
-            return this.contacts.find((el) => el.id === this.activeContactId)
+        activeContact() {
+            return this.contacts.find((el) => el.id === this.activeContactId);
         },
+        filteredContacts(){
+            return this.contacts.filter((el) => el.name.toLowerCase().includes(this.searchText.toLowerCase()));
+        }
     }
 }).mount('#app')
